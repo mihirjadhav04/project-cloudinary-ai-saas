666bf06b3a63dca24c6d18f8d77e7d2061f3679e
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-11-14 19:08:10 +0530

    created frontend routes and basic UI taken from dasy UI and used clerk

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,8 @@ import { NextResponse } from 'next/server';
 
 // Define public routes that don't require user authentication
 const isPublicRoute = createRouteMatcher([
-    "/signin",
-    "/signup",
+    "/sign-in",
+    "/sign-up",
     "/",
     "/home"
 ]);
@@ -15,32 +15,29 @@ const isPublicApiRoute = createRouteMatcher([
 ]);
 
 // Main middleware function using Clerk for authentication
-export default clerkMiddleware((auth, req) => {
-    const { userId } = auth();  // Get the user's ID if they're logged in
+export default clerkMiddleware(async (auth, req) => {
+    const { userId } = await auth();  // Get the user's ID if they're logged in
     const currentUrl = new URL(req.url);  // Get the current URL of the request
     const isAccessingDashboard = currentUrl.pathname === "/home";  // Check if the user is accessing the dashboard
     const isApiRequest = currentUrl.pathname.startsWith("/api");  // Check if the request is for an API route
 
-    // If the user is logged in and trying to access a public route that isn't the dashboard
+    console.log("USERID: ", userId);
+
+    // Redirect logged-in users away from public routes to the dashboard
     if (userId && isPublicRoute(req) && !isAccessingDashboard) {
-        // Redirect logged-in users away from public routes to the dashboard
         return NextResponse.redirect(new URL("/home", req.url));
     }
 
-    // If the user is not logged in
+    // If the user is not logged in, restrict access to non-public routes and non-public API routes
     if (!userId) {
-        // Redirect non-logged-in users trying to access private routes or non-public API routes to the signin page
-        if (!isPublicRoute(req) && isPublicApiRoute(req)) {
-            return NextResponse.redirect(new URL("/signin", req.url));
+        // If accessing a non-public route or a non-public API route, redirect to the sign-in page
+        if (!isPublicRoute(req) || (isApiRequest && !isPublicApiRoute(req))) {
+            return NextResponse.redirect(new URL("/sign-in", req.url));
         }
     }
 
-    // For API requests that are not marked as public, redirect to the signin page if the user is not logged in
-    if (isApiRequest && !isPublicApiRoute(req)) {
-        return NextResponse.redirect(new URL("/signin", req.url));
-    }
-
-    return NextResponse.next()
+    // Allow access if authenticated or accessing a public route
+    return NextResponse.next();
 });
 
 // Configuration for middleware to define which routes it should match and process
